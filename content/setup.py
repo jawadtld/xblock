8c873142b5fff563ba968b8c126b6ab941db0348
Muhammed Jawad <jawadtld@gmail.com>
2017-03-24 16:26:06 +0530

    updated h2 tags with main-header

--- content/setup.py
+++ content/setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='content-xblock',
-    version='8.5',
+    version='8.7',
     description='content XBlock',   # TODO: write a better description.
     license='UNKNOWN',          # TODO: choose a license: 'AGPL v3' and 'Apache 2.0' are popular.
     packages=[
